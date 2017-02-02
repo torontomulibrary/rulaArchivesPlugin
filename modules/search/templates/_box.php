d5d620be889505bcf2e78b68eabc5b1767cdc0dc
Patrick Fung <patrick@makestuffdostuff.com>
2017-02-02 13:21:21 -0500

    remove the weird dropdown menu..

--- modules/search/templates/_box.php
+++ modules/search/templates/_box.php
@@ -14,43 +14,6 @@
       <span class="caret"></span>
     </button>
 
-    <div class="btn-group">
-      <?php $cacheKey = 'search-box-nav-'.$sf_user->getCulture() ?>
-      <?php if (!cache($cacheKey)): ?>
-        <ul class="dropdown-menu pull-right">
-          <?php $icons = array(
-            'browseInformationObjects' => '/images/icons-large/icon-archival.png',
-            'browseActors' => '/images/icons-large/icon-people.png',
-            'browseRepositories' => '/images/icons-large/icon-institutions.png',
-            'browseSubjects' => '/images/icons-large/icon-subjects.png',
-            'browseFunctions' => '/images/icons-large/icon-functions.png',
-            'browsePlaces' => '/images/icons-large/icon-places.png',
-            'browseDigitalObjects' => '/images/icons-large/icon-media.png') ?>
-          <?php $browseMenu = QubitMenu::getById(QubitMenu::BROWSE_ID) ?>
-          <?php if ($browseMenu->hasChildren()): ?>
-            <?php foreach ($browseMenu->getChildren() as $item): ?>
-              <li>
-                <a href="<?php echo url_for($item->getPath(array('getUrl' => true, 'resolveAlias' => true))) ?>">
-                  <?php if (isset($icons[$item->name])): ?>
-                    <?php echo image_tag($icons[$item->name], array('width' => 42, 'height' => 42, 'alt' => '')) ?>
-                  <?php endif; ?>
-                  <?php echo esc_entities($item->getLabel(array('cultureFallback' => true))) ?>
-                </a>
-              </li>
-            <?php endforeach; ?>
-          <?php endif; ?>
-          <li class="divider"></li>
-          <li class="advanced-search">
-            <a href="<?php echo url_for(array('module' => 'informationobject', 'action' => 'browse', 'showAdvanced' => true, 'topLod' => false)) ?>">
-              <i class="icon-zoom-in"></i>
-              <?php echo __('Advanced search') ?>
-            </a>
-          </li>
-        </ul>
-        <?php cache_save($cacheKey) ?>
-      <?php endif; ?>
-    </div>
-
     <div id="search-realm" class="search-popover">
 
       <?php if (sfConfig::get('app_multi_repository')): ?>
